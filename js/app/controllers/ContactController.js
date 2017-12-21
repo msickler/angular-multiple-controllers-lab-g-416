b79922d3d04265d707b75495b374fcc8473ea24d
Mariana Sickler <marianaleal_93@hotmail.com>
2017-12-21 15:53:00 +0000

    automatically backed up by learn

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,8 +1,12 @@
 function ContactController() {
   var vm = this
-  vm.name =
-  vm.email =
-  vm.phone =
+  vm.name = "sample"
+  vm.email = "m@gmail.com"
+  vm.phone = "123-234-3345"
+
+  this.changeName = function() {
+    vm.name = "new name"
+  }
 }
 
 angular
